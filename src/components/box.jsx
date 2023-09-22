a5aaf0ad2a264d6e02aff9baf66c061cae2d718c
Riel <jasper.apos@gmail.com>
2023-09-22 22:23:49 +0800

    changed: not centered

--- src/components/box.jsx
+++ src/components/box.jsx
@@ -1,13 +1,13 @@
 function Box({ children }) {
     return (
-        <div className="flex items-center h-screen justify-center ml-64 p-8"> 
-            <div className="w-auto h-auto flex shadow-xl rounded-lg bg-white">
+        <div className="flex items-center p-4 justify-center ml-64">
+            <div className="w-auto h-auto max-h-screen overflow-y-auto pl-20 pr-20 flex shadow-xl rounded-lg bg-white">
                 <div className="p-8">
                     {children}
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Box;
