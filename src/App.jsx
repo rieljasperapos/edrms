1de4b062d1c065d86e8b2908cb5786a7892c01b6
Riel <jasper.apos@gmail.com>
2023-09-23 05:24:50 +0000

    TODO: Pass the dump datas as a props to the Box

--- src/App.jsx
+++ src/App.jsx
@@ -65,62 +65,59 @@ function App() {
                 </th>
               </tr>
             </thead>
-            </table>
-            <div className='max-h-screen overflow-y-auto'>
-              <table>
-                <tbody>
-                  {displayedData.map((item) => (
-                    <tr key={item.id}>
-                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                      <p>00{item.id}</p>
-                    </td>
-                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                      <p>Morales</p>
-                    </td>
-                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                      <p>Stanleigh</p>
-                    </td>
-                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                      <p>Balmes</p>
-                    </td>
-                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                      <p>19</p>
-                    </td>
-                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                      <p>0XXXXXXXXXXX</p>
-                    </td>
-                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                      <button className='bg-custom-blue text-white h-10 w-14 rounded-lg mr-2 mb-2'>View</button>
-                      <button className='bg-gray-300 h-10 w-14 rounded-lg'>Edit</button>
-                    </td>
-                  </tr>
+    
+            <tbody>
+              {displayedData.map((item) => (
+                <tr key={item.id}>
+                  <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                    <p>00{item.id}</p>
+                  </td>
+                  <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                    <p>Morales</p>
+                  </td>
+                  <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                    <p>Stanleigh</p>
+                  </td>
+                  <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                    <p>Balmes</p>
+                  </td>
+                  <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                    <p>19</p>
+                  </td>
+                  <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                    <p>0XXXXXXXXXXX</p>
+                  </td>
+                  <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                    <button className='bg-custom-blue text-white h-10 w-14 rounded-lg mr-2 mb-2'>View</button>
+                    <button className='bg-gray-300 h-10 w-14 rounded-lg'>Edit</button>
+                  </td>
+                </tr>
                   ))}
-                </tbody>
-              </table>
-
-              <ReactPaginate
-              pageCount={Math.ceil(yourData.length / itemsPerPage)}
-              pageRangeDisplayed={2}
-              marginPagesDisplayed={1}
-              onPageChange={handlePageChange}
-              forcePage={currentPage}
-              containerClassName="flex justify-center my-4"
-              pageClassName="inline-block mx-1 rounded-lg px-4 py-2 text-gray-600"
-              pageLinkClassName="text-sm"
-              activeClassName="bg-gray-200 text-white"
-              previousLabel={
-                <button className="hover:bg-custom-blue hover:text-white px-4 py-2 rounded-full mr-2">
-                  Previous
-                </button>
-              }
-              nextLabel={
-                <button className="hover:bg-custom-blue hover:text-white px-4 py-2 rounded-full">
-                  Next
-                </button>
-              }
-              />
-            </div>
+            </tbody>
+          </table>
         </div>
+
+        <ReactPaginate
+        pageCount={Math.ceil(yourData.length / itemsPerPage)}
+        pageRangeDisplayed={2}
+        marginPagesDisplayed={1}
+        onPageChange={handlePageChange}
+        forcePage={currentPage}
+        containerClassName="flex justify-center my-4"
+        pageClassName="inline-block mx-1 rounded-lg px-4 py-2 text-gray-600"
+        pageLinkClassName="text-sm"
+        activeClassName="bg-gray-200 text-white"
+        previousLabel={
+          <button className="hover:bg-custom-blue hover:text-white px-4 py-2 rounded-full mr-2">
+            Previous
+          </button>
+        }
+        nextLabel={
+          <button className="hover:bg-custom-blue hover:text-white px-4 py-2 rounded-full">
+            Next
+        </button>
+        }
+        />
       </Box>
         
       </div>
