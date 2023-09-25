2455868d949656ddff4236c2bd6a09cb624e2238
Riel <jasper.apos@gmail.com>
2023-09-25 13:55:27 +0000

    add: footer

--- src/components/box.jsx
+++ src/components/box.jsx
@@ -19,11 +19,11 @@ function Box({ children }) {
     return (
         <>
             <div className="bg-white bg-opacity-80 shadow-2xl h-24 w-full ml-64">
-                <div className="flex items-center text-2xl font-bold h-full uppercase ml-4">
+                <div className="flex items-center text-2xl font-bold h-full uppercase ml-8">
                     <h1>Patient Records</h1>
                 </div>
 
-                <div className="justify-center bg-white h-screen w-full p-8">
+                <div className="justify-center bg-white h-auto w-full p-8 pl-14 pr-14">
                     <div className='mt-4 flex justify-between'>
                         <div>
                             <p>Search</p>
@@ -34,7 +34,7 @@ function Box({ children }) {
                         </div>
                     </div>
 
-                    <div className="overflow-x-auto mt-4 flex-grow">
+                    <div className="overflow-x-auto mt-4">
                         <table className="min-w-full">
                             <thead>
                                 <tr>
@@ -69,19 +69,19 @@ function Box({ children }) {
                                             <p>00{item.id}</p>
                                         </td>
                                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                            <p>Morales</p>
+                                            <p>{item.lastName}</p>
                                         </td>
                                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                            <p>Stanleigh</p>
+                                            <p>{item.firstName}</p>
                                         </td>
                                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                            <p>Balmes</p>
+                                            <p>{item.middleName}</p>
                                         </td>
                                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                            <p>19</p>
+                                            <p>{item.age}</p>
                                         </td>
                                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                            <p>0XXXXXXXXXXX</p>
+                                            <p>{item.contactNumber}</p>
                                         </td>
                                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
                                             <button className='bg-custom-blue text-white h-10 w-20 rounded-lg mr-4 mb-2'>View</button>
@@ -102,7 +102,7 @@ function Box({ children }) {
                         containerClassName="flex justify-center my-4"
                         pageClassName="inline-block mx-1 rounded-lg px-4 py-2 text-gray-600"
                         pageLinkClassName="text-sm"
-                        activeClassName="bg-gray-200 text-white"
+                        activeClassName="bg-gray-200"
                         previousLabel={
                             <button className="hover:bg-custom-blue hover:text-white px-4 py-2 rounded-full mr-2">
                                 Previous
@@ -114,7 +114,9 @@ function Box({ children }) {
                             </button>
                         }
                     />
-
+                </div>
+                <div className='h-16 text-center flex flex-col justify-center bg-white bg-opacity-50'>
+                    <footer className='mt-0 opacity-80 text-sm'>Copyright @ 2023 edrms</footer>
                 </div>
             </div>
         </>
