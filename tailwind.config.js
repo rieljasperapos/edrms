347e84b56f43c2e1885c67d92fc5de176725871e
Riel <jasper.apos@gmail.com>
2023-10-27 09:58:11 +0800

    custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,14 @@ export default {
     extend: {
       backgroundColor: {
         'custom-blue': '#189AB4',
-      }
+        'custom-gray': '#F0F0F0',
+      },
+      fontFamily: {
+        'Monsterrat': ['Montserrat', 'sans-serif'],
+      },
+      boxShadow: {
+        'inner-dark': 'inset 2px 2px 6px 0 rgba(0, 0, 0, 0.2)',
+      },
     },
   },
   plugins: [],
