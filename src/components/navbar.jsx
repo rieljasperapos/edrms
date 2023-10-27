a2798c8773c30bd5c1aebadd5ad7a1e70414250f
Riel <jasper.apos@gmail.com>
2023-10-27 09:58:43 +0800

    refactor: menu

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Logo from '../assets/clamPascoLogo.png';
-import AddIcon from '../assets/add-icon-black.png';
-import CalendarIcon from '../assets/calendar-icon-black.png';
-import FolderIcon from '../assets/folderIconBlack.png';
-import LogoutIcon from '../assets/logout-icon-black.png';
+import Logo from '../assets/clinicLogo.png';
+import AddIcon from '../assets/add-icon.png';
+import CalendarIcon from '../assets/calendar-icon.png';
+import FolderIcon from '../assets/folder-icon.png';
+import LogoutIcon from '../assets/logout-icon.png';
 
 const Navbar = () => {
     return (
@@ -14,8 +14,8 @@ const Navbar = () => {
             <div className="flex-grow flex flex-col items-center justify-center bg-white text-black">
                 <div className="grid m-auto mt-8">
                     <NavItem icon={FolderIcon} text="Patient Records" />
+                    <NavItem icon={AddIcon} text="Add Record" />
                     <NavItem icon={CalendarIcon} text="Calendar" />
-                    <NavItem icon={AddIcon} text="Add Patient" />
                 </div>
                 <div id="menu-logout" className="cursor-pointer mt-auto mb-10 pb-8 flex space-x-2">
                     <img src={LogoutIcon} className="w-6 h-6" alt="Logout Icon" />
@@ -28,7 +28,7 @@ const Navbar = () => {
 const NavItem = ({ icon, text }) => {
     return (
         <div className="cursor-pointer pr-2 pl-2">
-            <div className="flex flex-col flex-wrap justify-center items-center p-2 hover:bg-gray-100 h-20 rounded-xl shadow-inner">
+            <div className="flex flex-col flex-wrap justify-center items-center p-2 hover:bg-custom-gray h-20 rounded-xl hover:shadow-inner-dark">
                 <img src={icon} className="w-6 h-6" alt={`${text} Icon`} />
                 <p className="text-sm text-center">{text}</p>
             </div>
