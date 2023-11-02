4349b6eef346c3fa391591e12d5bed4921810400
Jasper Apos <jasper.apos@gmail.com>
2023-11-02 03:56:43 +0000

    added custom-green color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ export default {
     extend: {
       backgroundColor: {
         'custom-blue': '#189AB4',
-        'custom-gray': '#F0F0F0',
+        'custom-gray': '#F9FAFB',
+        'primary-green': '#6ED178'
       },
       fontFamily: {
         'Monsterrat': ['Montserrat', 'sans-serif'],
@@ -16,6 +17,9 @@ export default {
       boxShadow: {
         'inner-dark': 'inset 2px 2px 6px 0 rgba(0, 0, 0, 0.2)',
       },
+      height: {
+        '95': '95vh'
+      }
     },
   },
   plugins: [],
