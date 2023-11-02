3597d90ba66da9660a63796f6e6d1bc50a814da2
hello-isa <isabeljoyadriatico@gmail.com>
2023-11-02 21:21:22 +0800

    Create Visit page

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import Login from "./pages/Login.jsx";
+import Visit from "./pages/Visit.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <App /> */}
-    <Login />
+    {/* <Login /> */}
+    <Visit />
   </React.StrictMode>
 );
