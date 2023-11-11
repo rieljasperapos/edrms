e438a4628eb6e574adf6a5a4277718572b5c0d1b
Jericho Pasco <112312834+Jpasco31@users.noreply.github.com>
2023-11-11 18:59:41 +0800

    Improve Design and tailwind-css for Add Rec

--- src/pages/AddRecord.jsx
+++ src/pages/AddRecord.jsx
@@ -8,93 +8,82 @@ function AddRecord() {
                 <h1 className="text-black font-bold font-Monsterrat text-3xl uppercase">Add Record</h1>
             </div>
 
-            <div className="p-4">
-                <div className="flex flex-col lg:flex-row justify-between bg-white px-8 flex-wrap">
-                    <div className="flex flex-row gap-x-16 justify-between flex-wrap">
-                        <div className="my-5 flex flex-col">
-                            <label className='mb-3'>Last Name</label>
-                            <input
-                                className=" pl-3 rounded-lg border-2 border-custom-blue h-12 w-96"
-                                type="text"
-                            />
-                        </div>
-                        <div className="my-5 flex flex-col">
-                            <label className='mb-3'> First Name</label>
-                            <input
-                                className="pl-3 rounded-lg border-2 border-custom-blue h-12 w-96"
-                                type="text"
-                            />
-                        </div>
-                        <div className="my-5 flex flex-col">
-                            <label className='mb-3'>Middle Name</label>
-                            <input
-                                className="pl-3 rounded-lg border-2 border-custom-blue h-12 w-96"
-                                type="text"
-                            />
-                        </div>
+            <div className="grid grid-cols-6 flex-col justify-between gap-x-6 gap-y-10 py-8 px-12">
+                    <div className="flex flex-col col-span-2 font-Karla text-lg">
+                        <label className='mb-3'>Last Name</label>
+                        <input
+                            className="pl-3 rounded-lg border-2 border-custom-blue h-12"
+                            type="text"
+                        />
                     </div>
-                    <div className="flex flex-row gap-x-16 flex-wrap">
-                        <div className="my-5 flex flex-col mr-56">
-                            <label className='mb-3'>BirthDate</label>
-                            <input
-                                className="px-3 rounded-lg border-2 border-custom-blue h-12 w-40"
-                                type="date"
-                            />
-                        </div>
-                        <div className="my-5 flex flex-col">
-                            <label className='mb-3'>Age</label>
-                            <input
-                                className="pl-3 rounded-lg border-2 border-custom-blue h-12 w-20"
-                                type="number"
-                            />
-                        </div>
-                        <div className="my-5 flex flex-col">
-                            <label className='mb-3'>Sex</label>
-                            <select id="countries" className="border-2 border-custom-blue w-60 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5">
-                                <option selected>Select</option>
-                                <option value="male">Male</option>
-                                <option value="female">Female</option>
-                            </select>
-                        </div>
+                    <div className="flex flex-col col-span-2 font-Karla text-lg">
+                        <label className='mb-3'> First Name</label>
+                        <input
+                            className="pl-3 rounded-lg border-2 border-custom-blue h-12"
+                            type="text"
+                        />
                     </div>
-                    <div className="flex flex-row gap-x-16 justify-between flex-wrap">
-                        <div className="my-5 flex flex-col">
-                            <label className='mb-3'>Address</label>
-                            <input
-                                className="pl-3 rounded-lg border-2 border-custom-blue h-12 w-216"
-                                type="text"
-                            />
-                        </div>
-                        <div className="my-5 flex flex-col">
-                            <label className='mb-3'>City</label>
-                            <input
-                                className="pl-3 rounded-lg border-2 border-custom-blue h-12 w-96"
-                                type="text"
-                            />
-                        </div>
+                    <div className="flex flex-col col-span-2 font-Karla text-lg">
+                        <label className='mb-3'>Middle Name</label>
+                        <input
+                            className="pl-3 rounded-lg border-2 border-custom-blue h-12"
+                            type="text"
+                        />
                     </div>
-                    <div className="flex flex-row gap-x-16 flex-wrap">
-                        <div className="my-5 flex flex-col">
-                            <label className='mb-3'>Contact Number</label>
-                            <input
-                                className="pl-3 rounded-lg border-2 border-custom-blue h-12 w-96"
-                                type="text"
-                            />
-                        </div>
-                        <div className="my-5 flex flex-col mb-16">
-                            <label className='mb-3'>Email</label>
-                            <input
-                                className="pl-3 rounded-lg border-2 border-custom-blue h-12 w-96"
-                                type="text"
-                            />
-                        </div>
+                    <div className="flex flex-col col-span-2 font-Karla text-lg">
+                        <label className='mb-3'>Birthdate</label>
+                        <input
+                            className="px-3 rounded-lg border-2 border-custom-blue h-12"
+                            type="date"
+                        />
                     </div>
-                    <div className='flex w-full justify-center mb-10'>
-                        <button className="rounded-lg border-2 h-14 w-60 font-bold text-2xl bg-custom-green hover:bg-green-600 text-white">
-                            Add
-                        </button>
+                    <div className="flex flex-col col-span-2 font-Karla text-lg">
+                        <label className='mb-3'>Age</label>
+                        <input
+                            className="pl-3 rounded-lg border-2 border-custom-blue h-12"
+                            type="number"
+                            min="0"
+                        />
                     </div>
-                </div>
+                    <div className="flex flex-col col-span-2 font-Karla text-lg">
+                        <label className='mb-3'>Sex</label>
+                        <select id="countries" className="border-2 border-custom-blue text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5">
+                            <option selected>Select</option>
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                        </select>
+                    </div>
+                    <div className="flex flex-col col-span-4 font-Karla text-lg">
+                        <label className='mb-3'>Address</label>
+                        <input
+                            className="pl-3 rounded-lg border-2 border-custom-blue h-12 "
+                            type="text"
+                        />
+                    </div>
+                    <div className="flex flex-col col-span-2 font-Karla text-lg">
+                        <label className='mb-3'>City</label>
+                        <input
+                            className="pl-3 rounded-lg border-2 border-custom-blue h-12"
+                            type="text"
+                        />
+                    </div>
+                    <div className="flex flex-col col-span-2 font-Karla text-lg">
+                        <label className='mb-3'>Contact Number</label>
+                        <input
+                            className="pl-3 rounded-lg border-2 border-custom-blue h-12"
+                            type="text"
+                        />
+                    </div>
+                    <div className="flex flex-col col-span-2 font-Karla text-lg">
+                        <label className='mb-3'>Email</label>
+                        <input
+                            className="pl-3 rounded-lg border-2 border-custom-blue h-12"
+                            type="text"
+                        />
+                    </div>
+                    <button className="rounded-lg border-2 h-12 w-40 font-bold font-Karla text-2xl bg-custom-green hover:bg-green-600 text-white col-span-2 place-self-end">
+                            Submit
+                    </button>
             </div>
         </>
     );
