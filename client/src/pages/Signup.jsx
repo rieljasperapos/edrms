f7bf164a4b273f51a63692b500c34ec299a5cbb6
Riel <jasper.apos@gmail.com>
2023-11-19 01:10:05 +0800

    feat: signup logic storing inputs to database

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -18,6 +18,34 @@ const Signup = () => {
         setShowPassword(!showPassword);
     }
 
+    const handleSignup = () => {
+        if (!userName || !password || !firstName || !lastName || !middleName || !birthDate) {
+            alert("All fields must be filled out");
+            return;
+        }
+
+        fetch(`http://localhost:3000/signup`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({userName, password, firstName, lastName, middleName, birthDate}),
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            alert("Successfully Registered");
+            console.log(data);
+        })
+        .catch(error => {
+            console.error(`Error registering user: ${error.message}`);
+        })
+    }
+
     useEffect(() => {
         if (password === confirmPassword && (password != '' || confirmPassword != '')) {
             setMatchStatus(true);
@@ -26,7 +54,7 @@ const Signup = () => {
         }
     }, [password, confirmPassword]);
 
-    console.log(matchStatus)
+    // console.log(matchStatus)
 
     return (
         <>
@@ -41,7 +69,7 @@ const Signup = () => {
                     <div className='flex justify-center items-center p-4'>
                         <div className='w-full'>
                             <p>Username</p>
-                            <input type='text' className='border p-2 w-full rounded-lg' onChange={(e) => setUserName(e.target.value)}></input>
+                            <input value={userName} type='text' className='border p-2 w-full rounded-lg' onChange={(e) => setUserName(e.target.value)}></input>
                         </div>
                     </div>
                     <div className='flex justify-center items-center p-4'>
@@ -51,7 +79,8 @@ const Signup = () => {
                                 {matchStatus && (<img className='h-6 ml-2' src={Matched}></img>)}
                             </div>
                             <div className='relative'>
-                                <input 
+                                <input
+                                    value={password}
                                     type={showPassword ? 'text' : 'password'} 
                                     className='border p-2 w-full rounded-lg' 
                                     onChange={(e) => setPassword(e.target.value)}
@@ -69,7 +98,8 @@ const Signup = () => {
                                 {matchStatus && (<img className='h-6 ml-2' src={Matched}></img>)}
                             </div>
                             <div className='relative'>
-                                <input 
+                                <input
+                                    value={confirmPassword}
                                     type={showPassword ? 'text' : 'password'} 
                                     className='border p-2 w-full rounded-lg' 
                                     onChange={(e) => setConfirmPassword(e.target.value)}
@@ -83,33 +113,33 @@ const Signup = () => {
                     <div className='flex flex-col sm:flex-row justify-center items-center p-4'>
                         <div className='w-full sm:mr-4 pb-4 sm:p-0'>
                             <p>First name</p>
-                            <input type='text' className='border p-2 w-full rounded-lg' onChange={(e) => setFirstName(e.target.value)}></input>
+                            <input value={firstName} type='text' className='border p-2 w-full rounded-lg' onChange={(e) => setFirstName(e.target.value)}></input>
                         </div>
                         <div className='w-full pt-4 sm:p-0'>
                             <p>Last name</p>
-                            <input type='text' className='border p-2 w-full rounded-lg' onChange={(e) => setLastName(e.target.value)}></input>
+                            <input value={lastName} type='text' className='border p-2 w-full rounded-lg' onChange={(e) => setLastName(e.target.value)}></input>
                         </div>
                     </div>
                     <div className='flex flex-col sm:flex-row justify-center items-center p-4'>
                         <div className='w-full sm:mr-4 pb-4 sm:p-0'>
                             <p>Middle name</p>
-                            <input type='text' className='border p-2 w-full rounded-lg' onChange={(e) => setMiddleName(e.target.value)}></input>
+                            <input value={middleName} type='text' className='border p-2 w-full rounded-lg' onChange={(e) => setMiddleName(e.target.value)}></input>
                         </div>
                         <div className='w-full pt-4 sm:p-0'>
                             <p>Birth Date</p>
-                            <input type="date" className='border p-2 w-full rounded-lg' onChange={(e) => setBirthDate(e.target.value)} placeholder='dd / mm / yy'></input>
+                            <input value={birthDate} type="date" className='border p-2 w-full rounded-lg' onChange={(e) => setBirthDate(e.target.value)} placeholder='dd / mm / yy'></input>
                         </div>
                     </div>
                     <div className='flex justify-center items-center p-4 mt-16'>
                         <div className='w-full'>
-                            <button className='bg-custom-blue hover:bg-opacity-60 transition-transform ease-in text-white p-4 rounded-lg w-full'>Sign up</button>
+                            <button className='bg-custom-blue hover:bg-opacity-60 transition-transform ease-in text-white p-4 rounded-lg w-full' onClick={handleSignup}>Sign up</button>
                             <div className='flex flex-row justify-center mt-4'>
                                 <div className='border-b h-2 w-64 mt-2'></div>
                                 <p className='text-center ml-4 mr-4'>or</p>
                                 <div className='border-b h-2 w-64 mt-2'></div>
                             </div>
                             <div className='flex justify-center items-center p-4'>
-                                <p>Already have an account? <span className='text-custom-blue cursor-pointer hover:text-opacity-60 transition-transform ease-in'>Signup</span></p>
+                                <p>Already have an account? <span className='text-custom-blue cursor-pointer hover:text-opacity-60 transition-transform ease-in'>Sign in</span></p>
                             </div>
                         </div>
                     </div>
--- server/server.js
+++ server/server.js
@@ -1,13 +1,51 @@
 const express = require('express');
 const connection = require('./db/db_connection');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
 
 const port = 3000;
 const app = express();
 app.use(cors());
+app.use(express.json());
+
+app.post('/signup', (req, res) => {
+    console.log(req.body);
+    if (!req.body.userName || !req.body.password || !req.body.firstName || !req.body.lastName || !req.body.middleName || !req.body.birthDate) {
+        res.send({message: "All fields must be filled out"});
+        return;
+    }
+
+    bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+        if (err) {
+            res.status(500).send({message: "Error hashing the password"});
+            return;
+        }
+
+        const sql = `INSERT INTO account (username, password, last_name, first_name, middle_name, birthdate) VALUES (?, ?, ?, ?, ?, ?)`;
+        connection.query(sql, [req.body.userName, hashedPassword, req.body.lastName, req.body.firstName, req.body.middleName, req.body.birthDate], (err, rows) => {
+            if (err) {
+                res.send({message: "Error registering a user"});
+            } else {
+                res.send({message: "Registered Successfully"});
+            }
+        })
+    })
+})
+
+app.get('/signup', (req, res) => {
+    const sql = `SELECT * FROM account`;
+    connection.query(sql, (err, rows) => {
+        res.send(rows);
+    })
+})
+
+app.get('/dashboard', (req, res) => {
+    res.send({ message: "Welcome to the dashboard!" });
+});
+
 
 app.get('/', (req, res) => {
-    res.send("Hello World")
+    res.send({message: "HELLO WORLD"});
 })
 
 app.listen(port, () => {
