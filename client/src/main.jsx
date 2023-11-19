33ec5a4c76f8ff1a3f642a3dba57d91ac1b28fac
Riel <jasper.apos@gmail.com>
2023-11-19 22:22:01 +0800

    added temporary dashboard

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
 import "./index.css";
 import App from "./App.jsx";
 import Login from "./pages/Login.jsx";
 import Visit from "./pages/Visit.jsx";
 import Signup from "./pages/Signup.jsx";
+import Dashboard from "./pages/dashboard.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
     {/* <Login /> */}
     {/* <Visit /> */}
-    <Signup />
+    {/* <Signup /> */}
   </React.StrictMode>
 );
