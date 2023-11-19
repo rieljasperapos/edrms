8cc67f7de512ba0f860c0684e39136d6c582416c
Riel <jasper.apos@gmail.com>
2023-11-19 22:21:41 +0800

    feat: routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
+import { useState, useEffect } from 'react';
 import Navbar from './components/navbar'
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/dashboard';
+import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
 
 function App() {
-
   return (
     <>
-      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/signin" element={<Login />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+        </Routes>
+    </BrowserRouter>
     </>
   )
 }
