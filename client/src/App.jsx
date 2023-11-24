15f01591b421da346e92965736345ad5aee4c847
Riel <jasper.apos@gmail.com>
2023-11-24 12:42:24 +0800

    feat: added calendar in the routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
-import { useState, useEffect } from 'react';
-import Navbar from './components/navbar'
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Dashboard from './pages/dashboard';
-import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
+import Calendar from './pages/calendarpage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route exact path='/calendar/*' element={<Calendar />} />
           <Route exact path="/signup" element={<Signup />} />
           <Route exact path="/signin" element={<Login />} />
           <Route exact path="/dashboard" element={<Dashboard />} />
