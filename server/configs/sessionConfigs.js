7f076cfea72e6e51dca451143f8b19fcc764e343
Riel <jasper.apos@gmail.com>
2023-12-05 22:23:39 +0800

    refactor: organize session configurations

---
+++ server/configs/sessionConfigs.js
@@ -0,0 +1,16 @@
+const session = require('express-session');
+const { v4: uuidv4 } = require('uuid');
+
+const sessionSecret = uuidv4();
+
+const sessionConfig = {
+    secret: sessionSecret,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: false,
+        maxAge: 60 * 60 * 1000,
+    },
+};
+
+module.exports = sessionConfig;
