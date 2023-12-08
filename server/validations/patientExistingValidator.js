c098ca8dfaa09a408c976512d64fa8e15787cbd6
Riel <jasper.apos@gmail.com>
2023-12-09 06:59:07 +0800

    comment unused codes

--- server/validations/patientExistingValidator.js
+++ server/validations/patientExistingValidator.js
@@ -1,15 +1,15 @@
-const connection = require('../db/db_connection');
-const express = require('express');
+// const connection = require('../db/db_connection');
+// const express = require('express');
 
-const checkPatientExist = (req, res) => {
-    const sql = `SELECT patient_id FROM patient WHERE CONCAT(last_name, ', ', first_name, ' ', middle_name) = ?`;
-    connection.query(sql, [req.body.name], (err, rows) => {
-        if (rows.length == 0) {
-            res.send({message: "Patient didnt exist", exist: false});
-        } else {
-            res.send({message: "Patient Exists", exist: true});
-        }
-    })
-}
+// const checkPatientExist = (req, res) => {
+//     const sql = `SELECT patient_id FROM patient WHERE CONCAT(last_name, ', ', first_name, ' ', middle_name) = ?`;
+//     connection.query(sql, [req.body.name], (err, rows) => {
+//         if (rows.length == 0) {
+//             res.send({message: "Patient didnt exist", exist: false});
+//         } else {
+//             res.send({message: "Patient Exists", exist: true});
+//         }
+//     })
+// }
 
-module.exports = { checkPatientExist };
+// module.exports = { checkPatientExist };
